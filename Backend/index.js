9ed9964ad6144f97a39327d2c27e6d4d529d1c0c
Trishna-sharma <trishnasharma2002@gmail.com>
2025-05-10 02:57:26 -0700

    fixes

--- Backend/index.js
+++ Backend/index.js
@@ -6,7 +6,12 @@ const cors = require("cors")
 const app = express();
 app.use(cors())
 const port = 8000;
-app.use(express.json())
+const corsOptions = {
+    origin: ["http://localhost:5173", "https://journal-app-frontend.vercel.app"], // Add your frontend URLs here
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+    credentials: true, // Allow cookies if needed
+};
+app.use(express.json(),cors(corsOptions))
 
 app.listen(port, () => {
     console.log(`Server live at http://localhost:${port}`)
--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,4 +1,4 @@
-export const REACT_APP_BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+export const REACT_APP_BACKEND_URL = "https://journal-app-backend-phi.vercel.app";
 export const JWT_SECRET = "____"
 
 
